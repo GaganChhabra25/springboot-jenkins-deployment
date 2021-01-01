06279a7c810c19815cafe40791a9cb7f79fb6641
Gagan Chhabra <gagan.chhabra1990@gmail.com>
2021-01-01 14:47:09 +0530

    initial-commit

---
+++ src/main/java/com/gagan/test/TestApplication.java
@@ -0,0 +1,12 @@
+package com.gagan.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class TestApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
---
+++ src/main/java/com/gagan/test/controllers/WelcomeController.java
@@ -0,0 +1,21 @@
+package com.gagan.test.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/hello")
+    public Collection<String> sayHello() {
+        return IntStream.range(0, 10)
+                .mapToObj(i -> "Hello number " + i)
+                .collect(Collectors.toList());
+    }
+}
